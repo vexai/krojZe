565ec94c7befe24d18de6b62476cf2466d7499ec
vexai <endove3@gmail.com>
2020-05-04 13:30:20 +0200

    gh-pages2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
     <div className="App">
       <Nav />
       <Switch>
